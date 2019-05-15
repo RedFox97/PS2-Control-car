5378c6f4b7af8eac01737a1f089a5dba9aa71776
RedFox97 <46229814+RedFox97@users.noreply.github.com>
2019-05-15 14:50:21 +0700

    Update PS2_Test.ino

--- PS2_Test/PS2_Test.ino
+++ PS2_Test/PS2_Test.ino
@@ -8,46 +8,59 @@
 
 #include <PS2X_lib.h>
 
-#define PS2_DAT       6       // Data Pin
-#define PS2_CMD       7       // Command Pin
-#define PS2_CS        8       // Attention Pin
-#define PS2_CLK       9       // Clock Pin
+#define PS2_DAT     8       // Data Pin
+#define PS2_CMD     9       // Command Pin
+#define PS2_CS      10      // Attention Pin
+#define PS2_CLK     11      // Clock Pin
 
-PS2X ps2;                     // create PS2 Controller Class
+PS2X ps2;                   // create PS2 Controller Class
 
 void setup() {
   Serial.begin(9600);
-  // Khởi tạo kết nối tay PS2
-  ps2.config_gamepad(PS2_CLK, PS2_CMD, PS2_CS, PS2_DAT);
+  delay(300);
+  // setup pins and settings: GamePad(clock, command, attention, data) check for error
+  int ps2Config = ps2.config_gamepad(PS2_CLK, PS2_CMD, PS2_CS, PS2_DAT);
 }
 
 void loop() {
-  // Bắt đầu đọc dữ liệu nhấn nút từ tay PS2
   ps2.read_gamepad();
-
   if(ps2.ButtonPressed(PSB_PAD_UP))
-    Serial.println("Button UP Press");
-  if(ps2.ButtonPressed(PSB_PAD_LEFT))
-    Serial.println("Button LEFT Press");
-  if(ps2.ButtonPressed(PSB_PAD_RIGHT))
-    Serial.println("Button RIGHT Press");
+    Serial.println("Press UP");
   if(ps2.ButtonPressed(PSB_PAD_DOWN))
-    Serial.println("Button DOWN Press");
-    
-  if(ps2.ButtonPressed(PSB_BLUE))
-    Serial.println("Button TRIANGLE Press");
-  if(ps2.ButtonPressed(PSB_PINK))
-    Serial.println("Button CROSS Press");
-  if(ps2.ButtonPressed(PSB_RED))
-    Serial.println("Button CIRCLE Press");
-  if(ps2.ButtonPressed(PSB_GREEN))
-    Serial.println("Button SQUARE Press");
+    Serial.println("Press DOWN");
+  if(ps2.ButtonPressed(PSB_PAD_LEFT))
+    Serial.println("Press LEFT");
+  if(ps2.ButtonPressed(PSB_PAD_RIGHT))
+    Serial.println("Press RIGHT");
+  if(ps2.ButtonPressed(PSB_TRIANGLE))
+    Serial.println("Press Y");
+  if(ps2.ButtonPressed(PSB_CIRCLE))
+    Serial.println("Press B");
+  if(ps2.ButtonPressed(PSB_CROSS))
+    Serial.println("Press A");
+  if(ps2.ButtonPressed(PSB_SQUARE))
+    Serial.println("Press X");
   if(ps2.ButtonPressed(PSB_L1))
-    Serial.println("Button L1 Press");
+    Serial.println("Press L1");
   if(ps2.ButtonPressed(PSB_L2))
-    Serial.println("Button L2 Press");
+    Serial.println("Press L2");
+  if(ps2.ButtonPressed(PSB_L3))
+    Serial.println("Press L3");
   if(ps2.ButtonPressed(PSB_R1))
-    Serial.println("Button R1 Press");
+    Serial.println("Press R1");
   if(ps2.ButtonPressed(PSB_R2))
-    Serial.println("Button R2 Press");
+    Serial.println("Press R2");
+  if(ps2.ButtonPressed(PSB_R3))
+    Serial.println("Press R3");
+  if(ps2.Button(PSB_L1) || ps2.Button(PSB_R1)) { //print stick values if either is TRUE
+    Serial.print("Stick Values:");
+    Serial.print(ps2.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
+    Serial.print(",");
+    Serial.print(ps2.Analog(PSS_LX), DEC); 
+    Serial.print(",");
+    Serial.print(ps2.Analog(PSS_RY), DEC);
+    Serial.print(",");
+    Serial.println(ps2.Analog(PSS_RX), DEC); 
+  }     
+  delay(50);
 }
